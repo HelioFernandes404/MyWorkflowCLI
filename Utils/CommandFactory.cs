641c7eb607c29d7a006794c4ed58f96f4644eba8
HelioFernandes404 <heliojobs25@gmail.com>
2024-07-24 11:23:35 -0300

    fix: adicionando mudanças para o Factory Method

---
+++ Utils/CommandFactory.cs
@@ -0,0 +1,28 @@
+﻿using MyWorkflow.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkflow.Utils
+{
+    public static class CommandFactory
+    {
+        public static ICommand CreateCommand(string command)
+        {
+            return command switch
+            {
+                "-h" => new HelpCommand(),
+                "--help" => new HelpCommand(),
+                "docker" => new DockerCommand(),
+                "linux" => new LinuxCommand(),
+                "psql" => new PsqlCommand(),
+                "pmc" => new PmcCommand(),
+                "git" => new GitCommand(),
+                "docs" => new DocsCommand(),
+                _ => null,
+            };
+        }
+    }
+}
