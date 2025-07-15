b468bd8bfb4966b08cc0dec09666dfe32c3b885b
heliofernandes404 <heliodevhub@gmail.com>
2025-07-15 11:48:25 -0300

    remove: remove codebase in c#

--- build.sh
+++
@@ -1,5 +0,0 @@
-# Windows
-dotnet publish -c Release -r win-x64 --self-contained
-
-# Linux
-dotnet publish -c Release -r linux-x64 --self-contained
--- src/utils/command_factory.py
+++ src/utils/command_factory.py
@@ -1,1 +1,33 @@
 # command_factory.py
+# This is a command factory that registers commands dynamically
+from abc import ABC, abstractmethod
+from typing import Optional, List
+
+class ICommand(ABC):
+    """Interface for all commands"""
+
+    @abstractmethod
+    def execute(self, args: List[str]) -> None:
+        """Execute the command with given arguments"""
+        pass
+
+class CommandFactory:
+    """Factory class to create command instances"""
+
+    @staticmethod
+    def create_command(command_name: str) -> Optional[ICommand]:
+        """Create a command instance based on the command name string"""
+        # import commands here to avoid circular imports
+        from commands.help_command import HelpCommand
+        from commands.default_command import DefaultCommand
+        from commands.git_command import GitCommand
+
+
+        command_map = {
+            "-h": HelpCommand,
+            "git": GitCommand
+            # Add other commands here
+        }
+
+        command_class = command_map.get(command_name.lower(), DefaultCommand)
+        return command_class()
