95fc43f5d34e01e952c2adc40c57a3127629109b
heliofernandes404 <heliodevhub@gmail.com>
2025-07-17 11:24:43 -0300

    refactor: struture show_help the argocd

--- src/commands/argocd_command.py
+++ src/commands/argocd_command.py
@@ -8,24 +8,22 @@ class ArgoCdCommand(ICommand):
             self._show_help()
             return
         
-        print("🔧 Executando comandos Argocd...")
-        print("Top 10 comandos mais usados no ArgoCD CLI:")
-        print("  1. argocd login <SERVER>")
-        print("  2. argocd app list")
-        print("  3. argocd app get <APPNAME>")
-        print("  4. argocd app sync <APPNAME>")
-        print("  5. argocd app create <APPNAME> --repo <REPO> --path <PATH> --dest-server <SERVER> --dest-namespace <NAMESPACE>")
-        print("  6. argocd app delete <APPNAME>")
-        print("  7. argocd app rollback <APPNAME> <REVISION>")
-        print("  8. argocd app history <APPNAME>")
-        print("  9. argocd app diff <APPNAME>")
-        print(" 10. argocd proj list")
+        self._show_help()
+
+    def workflow_command(self) -> None:        
+        pass
         
     def _show_help(self):
         """
         Displays help information for the Argocd command, including usage instructions and available options.
         """
-        print("Argocd Command Help")
-        print("Usage: wk argocd [options]")
-        print("Options:")
-        print("  --help, -h    Show this help")
+        print("argocd login <SERVER>")
+        print("argocd app list")
+        print("argocd app get <APPNAME>")
+        print("argocd app sync <APPNAME>")
+        print("argocd app create <APPNAME> --repo <REPO> --path <PATH> --dest-server <SERVER> --dest-namespace <NAMESPACE>")
+        print("argocd app delete <APPNAME>")
+        print("argocd app rollback <APPNAME> <REVISION>")
+        print("argocd app history <APPNAME>")
+        print("argocd app diff <APPNAME>")
+        print("argocd proj list")
