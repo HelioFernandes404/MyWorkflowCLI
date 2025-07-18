4edd1a7357d5989ee3883f6cd60e20190a5ad1ef
heliofernandes404 <heliodevhub@gmail.com>
2025-07-18 00:21:39 -0300

    feat: add new commands in git

--- src/commands/git_command.py
+++ src/commands/git_command.py
@@ -5,21 +5,15 @@ from utils.command_factory import ICommand
 class GitCommand(ICommand):
     def execute(self, args: List[str]) -> None:
         if len(args) > 1 and args[1] in ["use -h to show commands"]:
-            self._show_help()
             return
         
-        print("🔧 Executando comandos Git...")
-        # Your Git logic here
-        print("Comandos Git disponíveis:")
-        print("  - git status")
-        print("  - git add .")
-        print("  - git commit -m 'message'")
-        print("  - git push")
-        
-    def _show_help(self):
         print("Git Command Help")
-        print("Usage: myworkflow git [options]")
+        print("Usage: wk git [options]")
         print("Options:")
-        print("  --help, -h    Show this help")
-        print("  --status      Show git status")
-        print("  --commit      Make a commit")
+        print("  --help, -h                               Show this help")
+        print("  --status                                 Show git status")
+        print("  --commit                                 Make a commit")
+        print("  --stash push                             Push changes to stash")
+        print("  --stash list                             List all stashed changes")
+        print("  --stash pop                              Pop changes from stash")
+        print(" git config --global --list | grep alias   Show all alias configs")
